0f21237854c6810a13d16fc7535fb1714096e864
Hebron George <hebrontgeorge@gmail.com>
2019-10-17 22:06:57 -0500

    refactor message to a separate method

--- lib/happy_hood/slack/client.rb
+++ lib/happy_hood/slack/client.rb
@@ -6,9 +6,19 @@ module HappyHood
         two_day_valuation   = house_prices_on(2.days.ago)
         one_day_valuation   = house_prices_on(1.days.ago)
 
-        formatted_three_day_valuation = Kernel::sprintf("%.2f", house_prices_on(3.days.ago))
-        formatted_two_day_valuation   = Kernel::sprintf("%.2f", house_prices_on(2.days.ago))
-        formatted_one_day_valuation   = Kernel::sprintf("%.2f", house_prices_on(1.days.ago))
+        format_message(one_day_valuation, two_day_valuation, three_day_valuation)
+      end
+
+      private
+
+      def self.house_prices_on(date)
+        HousePrice.on(date).sum(:price)
+      end
+
+      def self.format_message(one_day_valuation, two_day_valuation, three_day_valuation)
+        formatted_three_day_valuation = Kernel::sprintf("%.2f", three_day_valuation)
+        formatted_two_day_valuation   = Kernel::sprintf("%.2f", two_day_valuation)
+        formatted_one_day_valuation   = Kernel::sprintf("%.2f", one_day_valuation)
 
         formatted_two_day_diff = Kernel::sprintf("%.2f",three_day_valuation-two_day_valuation)
         formatted_one_day_diff = Kernel::sprintf("%.2f",two_day_valuation-one_day_valuation)
@@ -16,12 +26,6 @@ module HappyHood
         message = "Three Days Ago: $#{formatted_three_day_valuation}\n" \
         "Two Days Ago: $#{formatted_two_day_valuation} ($#{formatted_two_day_diff})\n" \
         "One Day Ago: $#{formatted_one_day_valuation} ($#{formatted_one_day_diff})"
-      end
-
-      private
-
-      def self.house_prices_on(date)
-        HousePrice.on(date).sum(:price)
       end
     end
   end
