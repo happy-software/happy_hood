54f8c738546d12f8b301d08d57b5127f91c81eb3
Hebron George <hebrontgeorge@gmail.com>
2019-11-25 09:09:12 -0600

    Helper method to get hood valuation

--- app/models/hood.rb
+++ app/models/hood.rb
@@ -2,4 +2,6 @@ class Hood < ApplicationRecord
   has_many :houses
   has_many :home_owners_associations
 
+  scope :valuation_on, -> (date) { houses.map { |h| h.house_prices.on(date).last&.price }.compact.sum }
+
 end
