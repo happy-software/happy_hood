3691988d86b1aa91e983bede34086ad447a9dc17
Hebron George <hebrontgeorge@gmail.com>
2021-05-08 13:12:00 -0400

    Add a route to get valuation history and a simple test

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'pages#home'
+  get  '/house_valuations/:zpid', to: 'houses#valuations'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
