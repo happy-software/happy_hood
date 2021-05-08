286dd694dedcd1042fab47b285406551dcfd673c
Hebron George <hebrontgeorge@gmail.com>
2021-05-07 22:44:32 -0400

    Convert valuation_on into an instance method instead of a scope

--- app/models/hood.rb
+++ app/models/hood.rb
@@ -2,6 +2,11 @@ class Hood < ApplicationRecord
   has_many :houses
   has_many :home_owners_associations
 
-  scope :valuation_on, -> (date) { houses.map { |h| h.price_history.dig(date.strftime("%Y-%m-%d")).to_f }.compact.sum }
 
+  def valuation_on(date)
+    houses.map do |h|
+      normalized_date = date.strftime("%Y-%m-%d")
+      h.price_history.dig(normalized_date).to_f
+    end.compact.sum
+  end
 end
