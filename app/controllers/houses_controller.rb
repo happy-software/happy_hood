42e414e1f86f6e61f5f61401812048c8fce52222
Hebron George <hebrontgeorge@gmail.com>
2021-05-09 12:05:39 -0400

    Create Endpoint to Import Valuations

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,10 +1,41 @@
 class HousesController < ApplicationController
   def valuations
-    zpid = params[:zpid]
-    house_details = HouseMetadatum.find_by_zpid(zpid)
     return render json: {data: "Could not find a house with zpid: #{zpid}"}, status: 404 unless house_details
 
-    house = house_details.house
     render json: {data: house.price_history}, status: 200
+  end
+
+  def import
+    return render json: {data: "Could not find a house with zpid: #{zpid}"}, status: 404 unless house_details
+    return render json: {data: "Missing or improper valuation data"}, status: 400 unless valuations_param
+
+    # This data write could be moved into it's own module or worker or something but whatever, getting it working for now
+    price_history = house.price_history || {}
+    valuations_param.each do |date, price|
+      normalized_date = Date.parse(date).strftime("%Y-%m-%d")
+      price_history[normalized_date] ||= price.to_f
+    end
+    house.price_history = price_history
+    house.save!
+
+    render json: {data: "Successfully imported valuation history!"}, status: 200
+  end
+
+  private
+
+  def zpid
+    params[:zpid]
+  end
+
+  def valuations_param
+    params[:valuations]
+  end
+
+  def house_details
+    @house_details ||= HouseMetadatum.find_by_zpid(zpid)
+  end
+
+  def house
+    @house ||= house_details&.house
   end
 end
