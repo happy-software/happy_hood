1cefcb0ff15924a304222f5145ef457a17efc436
Hebron George <hebrontgeorge@gmail.com>
2021-05-15 01:46:37 -0400

    Move difference calc logic into it's own service and reject any that have no valuation difference

---
+++ app/services/daily_difference_calculator.rb
@@ -0,0 +1,33 @@
+class DailyDifferenceCalculator
+  attr_reader :hoods
+
+  def initialize(hoods)
+    @hoods  = hoods
+  end
+
+  def differences
+    hoods.map do |hood|
+      difference_details(hood)
+    end
+  end
+
+  private
+
+  HoodSummary = Struct.new(:name, :yesterdays_valuation, :todays_valuation, :valuation_difference, :house_count, :valuation_date)
+
+  def difference_details(hood)
+    yesterdays_valuation = hood.valuation_on(Date.yesterday)
+    todays_valuation     = hood.valuation_on(Date.today)
+    difference           = todays_valuation - yesterdays_valuation
+
+    HoodSummary.new(
+      hood.name,
+      yesterdays_valuation,
+      todays_valuation,
+      difference,
+      hood.houses.count,
+      Date.today
+    )
+  end
+
+end
