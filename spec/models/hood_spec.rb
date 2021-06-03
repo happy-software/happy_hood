f5a0b61b7a7849ba6d684a0f31eba66053060b93
Hector Rios <that.hector@gmail.com>
2021-06-03 18:12:38 -0500

    Introduce latest_valuation
    This commit adds logic to figure out what the latest valuation for a neighborhood is.
    
    The approach that we went with is to try to find a date when all the
    properties a neighborhood has have a valuation. Some things to look out
    for include:
    
    Hood 1:
      - House 1 (created 04-01), last valuated (05-01)
      - House 2 (created 04-01), last valuated (05-01)
      - House 3 (created 04-01), last valuated (05-01)
      - House 4 (created 05-01), not valuated
    
    If we invoke `latest_valuation` after `05-01`, we will not receive an
    amount because house 4 has not yet been valuated.
    
    This is a rigorous approach, but it's the simplest for now.

---
+++ spec/models/hood_spec.rb
@@ -0,0 +1,39 @@
+require "rails_helper"
+
+describe Hood do
+  describe "#valuation_before" do
+    context "when there are no houses" do
+      it "returns 0" do
+        expect(Hood.new.valuation_before(Date.today)).to eq({ date: nil, valuation: 0 })
+      end
+    end
+
+    context "when there are houses" do
+      context "when the houses were valuated yesterday" do
+        it "returns the sum of yesterdays valuation" do
+          hood = Hood.create
+          hood.houses.create(price_history: { 1.day.ago.strftime(Hood::PRICE_HISTORY_DATE_FORMAT) => 30, 3.days.ago.strftime(Hood::PRICE_HISTORY_DATE_FORMAT) => 5 })
+          hood.houses.create(price_history: { 1.day.ago.strftime(Hood::PRICE_HISTORY_DATE_FORMAT) => 20, 3.days.ago.strftime(Hood::PRICE_HISTORY_DATE_FORMAT) => 5 })
+
+          expect(hood.valuation_before(Date.today)).to eq({
+            date: 1.day.ago.to_date,
+            valuation: 30 + 20,
+          })
+        end
+      end
+
+      context "when only some of the houses were valuated yesterday" do
+        it "returns the sum of the valuation on the date they were all valuated" do
+          hood = Hood.create
+          hood.houses.create(price_history: { 3.days.ago.strftime(Hood::PRICE_HISTORY_DATE_FORMAT) => 5 })
+          hood.houses.create(price_history: { 1.day.ago.strftime(Hood::PRICE_HISTORY_DATE_FORMAT) => 20, 3.days.ago.strftime(Hood::PRICE_HISTORY_DATE_FORMAT) => 5 })
+
+          expect(hood.valuation_before(Date.today)).to eq({
+            date: 3.days.ago.to_date,
+            valuation: 5 + 5,
+          })
+        end
+      end
+    end
+  end
+end
