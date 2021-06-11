2dd9a9bf3f2e63ec67787a02d56d848a7229d706
Hebron George <hebrontgeorge@gmail.com>
2021-06-11 14:40:29 -0400

    Delete related house metadata when deleting a house

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,7 +1,7 @@
 class House < ApplicationRecord
   belongs_to :hood
   has_many :home_owners_associations, through: :hood
-  has_one :house_metadatum
+  has_one :house_metadatum, dependent: :destroy
 
   PRICE_HISTORY_DATE_FORMAT = "%Y-%m-%d".freeze
 
