d73796e2c70b77c8215804b18df397054e1e3182
Hector Rios <that.hector@gmail.com>
2021-06-12 11:38:58 -0500

    Add specs for the difference calculator

--- spec/services/difference_calculator_spec.rb
+++ spec/services/difference_calculator_spec.rb
@@ -12,35 +12,9 @@ describe DifferenceCalculator do
       it "generates a list of HoodDifference" do
         hoods = 3.times.map { |_| Hood.new }
 
-        expect(described_class.new(hoods, start_date: nil, end_date: nil).differences).to all(be_an(HoodDifference))
-      end
-
-      it "generates a proper summary" do
-        hood = Hood.create(name: "Schitt's Creek")
-        hood.houses.create(price_history: {
-          1.day.ago.strftime("%Y-%m-%d") => 300_000,
-          Date.today.strftime("%Y-%m-%d") => 310_000,
-        })
-        hood.houses.create(price_history: {
-          1.day.ago.strftime("%Y-%m-%d") => 280_000,
-          Date.today.strftime("%Y-%m-%d") => 287_000,
-        })
-
-        instance = described_class.new([hood])
-
-        differences = instance.differences
-
-        expect(differences.size).to eq(1)
-
-        difference = differences.first
-
-        expect(difference).to have_attributes(
-          hood_name: "Schitt's Creek",
-          earliest_valuation: 580_000.00,
-          latest_valuation: 597_000.00,
-          house_count: 2,
-          average_house_difference: 8_500.00
-        )
+        differences = described_class.new(hoods, start_date: nil, end_date: nil).differences
+        expect(differences).to all(be_an(HoodDifference))
+        expect(differences.size).to eq(3)
       end
     end
   end
