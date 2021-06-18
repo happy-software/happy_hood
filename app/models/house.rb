ae8e16244a746f34908d543290b8c7dcf702c564
Hector Rios <that.hector@gmail.com>
2021-06-18 12:20:49 -0500

    Add valuation helper to House
    This commit adds a method to more easily add pricing.

--- app/models/house.rb
+++ app/models/house.rb
@@ -5,8 +5,23 @@ class House < ApplicationRecord
 
   PRICE_HISTORY_DATE_FORMAT = "%Y-%m-%d".freeze
 
+  after_initialize :set_price_history
+
   def valuation_on(date)
     normalized_date = date.strftime(PRICE_HISTORY_DATE_FORMAT)
     price_history.dig(normalized_date).to_f
+  end
+
+  def add_valuation(date, price)
+    normalized_valuation_date = date.strftime(PRICE_HISTORY_DATE_FORMAT)
+    price_history[normalized_valuation_date] = price.to_f
+
+    self
+  end
+
+  private
+
+  def set_price_history
+    self.price_history ||= {}
   end
 end
---
+++ spec/models/house_spec.rb
@@ -0,0 +1,38 @@
+require "rails_helper"
+
+describe House do
+  describe "#valuation_on" do
+    it "correctly returns a proper valuation" do
+      date = Date.today
+      house = House.new
+      house.price_history[date.strftime(House::PRICE_HISTORY_DATE_FORMAT)] = 20
+
+      expect(house.valuation_on(1.day.ago)).to eq(0)
+      expect(house.valuation_on(date)).to eq(20)
+    end
+  end
+
+  describe "#add_valuation" do
+    context "when there hasn't been a valuation" do
+      it "creates a valuation" do
+        house = House.new
+        house.add_valuation(Date.today, 20)
+
+        expect(house.valuation_on(Date.today)).to eq(20)
+      end
+    end
+
+    context "when two updates happen on the same day" do
+      it "keeps the last update" do
+        date = Date.today
+
+        house = House.new
+        house.add_valuation(date, 20)
+        house.add_valuation(date, 10)
+        house.add_valuation(date, 42)
+
+        expect(house.valuation_on(date)).to eq(42)
+      end
+    end
+  end
+end
