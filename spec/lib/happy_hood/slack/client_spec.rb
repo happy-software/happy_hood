5ed7380a090f7090a983d6ca83db36e651e659b1
Hector Rios <that.hector@gmail.com>
2021-07-10 12:15:34 -0500

    Decouple rendering logic from slack client spec

--- spec/lib/happy_hood/slack/client_spec.rb
+++ spec/lib/happy_hood/slack/client_spec.rb
@@ -26,30 +26,13 @@ describe HappyHood::Slack::Client do
     
     context "when there is a summary available" do
       it "posts a message with the differences" do
-        mock_difference = instance_double(HoodDifference,
-                                          empty?: false,
-                                          hood_name: "Schitt's Creek",
-                                          earliest_valuation: 5.00,
-                                          earliest_valuation_date: 1.day.ago.to_date,
-                                          latest_valuation: 10.00,
-                                          latest_valuation_date: Date.today,
-                                          house_count: 1,
-                                          valuation_difference: 5.00,
-                                          average_house_difference: nil,
-                                          average_house_price: 10.00,
-                                         )
-        mock_calculator = instance_double(DifferenceCalculator, differences: [mock_difference])
-        allow(DifferenceCalculator).to receive(:new).and_return(mock_calculator)
+        mocked_response = "A ton of houses updated!"
+        allow(DifferenceRenderer).to receive(:summarize_differences).and_return(mocked_response)
 
         described_class.send_daily_price_summary
 
         expect(mock_slack_client).to have_received(:chat_postMessage).with(a_hash_including(
-          text: a_string_including(
-            "Schitt's Creek",
-            "#{1.day.ago.strftime(DifferenceRenderer::SHORT_DATE_FORMAT)}:    $5.00",
-            "#{Date.today.strftime(DifferenceRenderer::SHORT_DATE_FORMAT)}:    $10.00",
-            "Difference:      $5.00",
-          )
+          text: mocked_response,
         ))
       end
     end
@@ -79,30 +62,13 @@ describe HappyHood::Slack::Client do
     
     context "when there is a summary available" do
       it "posts a message with the differences" do
-        mock_difference = instance_double(HoodDifference,
-                                          empty?: false,
-                                          hood_name: "Schitt's Creek",
-                                          earliest_valuation: 5.00,
-                                          earliest_valuation_date: 1.month.ago.to_date,
-                                          latest_valuation: 10.00,
-                                          latest_valuation_date: Date.today,
-                                          house_count: 1,
-                                          valuation_difference: 5.00,
-                                          average_house_price: 10.00,
-                                          average_house_difference: nil
-                                         )
-        mock_calculator = instance_double(DifferenceCalculator, differences: [mock_difference])
-        allow(DifferenceCalculator).to receive(:new).and_return(mock_calculator)
+        mocked_response = "A ton of houses updated!"
+        allow(DifferenceRenderer).to receive(:summarize_differences).and_return(mocked_response)
 
-        described_class.send_daily_price_summary
+        described_class.send_monthly_price_summary
 
         expect(mock_slack_client).to have_received(:chat_postMessage).with(a_hash_including(
-          text: a_string_including(
-            "Schitt's Creek",
-            "#{1.month.ago.strftime(DifferenceRenderer::SHORT_DATE_FORMAT)}:    $5.00",
-            "#{Date.today.strftime(DifferenceRenderer::SHORT_DATE_FORMAT)}:    $10.00",
-            "Difference:      $5.00",
-          )
+          text: mocked_response
         ))
       end
     end
