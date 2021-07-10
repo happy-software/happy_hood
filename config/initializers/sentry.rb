3f6e201283bef28492b1daca0fb4338ff984f1cc
Hector Rios <that.hector@gmail.com>
2021-07-10 13:05:48 -0500

    Add Sentry configuration as separate file

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,6 +1,6 @@
 Sentry.init do |config|
   # if this environment variable is set, we don't need to have this file
   # however, we're leaving it here to allow us to customize any settings for Sentry in the future
-  config.dsn = ENV["SENTRY_DSN"]
-  config.environment = Rails.env
+  config.dsn = Configuration::Sentry.dsn
+  config.environment = Configuration::Sentry.environment
 end
