c832693f0641b488326efd78c1f900bd9f5ffc6c
Hector Rios <that.hector@gmail.com>
2021-07-10 12:20:23 -0500

    Move defaults into slack method

--- lib/happy_hood/slack/client.rb
+++ lib/happy_hood/slack/client.rb
@@ -6,13 +6,22 @@ module HappyHood
 
       def self.send_daily_price_summary
         message = daily_message
-        slack.chat_postMessage(message)
+
+        slack.chat_postMessage({
+          text: message,
+          icon_emoji: DefaultIconEmoji,
+          channel: DefaultSlackChannel,
+        })
       end
 
       def self.send_monthly_price_summary
         message = monthly_message
 
-        slack.chat_postMessage(message)
+        slack.chat_postMessage({
+          text: message,
+          icon_emoji: DefaultIconEmoji,
+          channel: DefaultSlackChannel,
+        })
       end
 
       private
@@ -25,26 +34,14 @@ module HappyHood
         neighborhood_differences = DifferenceCalculator.new(Hood.all, start_date: 1.day.ago, end_date: Date.today).differences
         messages  = DifferenceRenderer.summarize_differences(neighborhood_differences)
 
-        messages = messages.empty? ? 'No changes for any HappyHood' : messages
-
-        {
-          text:       messages,
-          icon_emoji: DefaultIconEmoji,
-          channel:    DefaultSlackChannel,
-        }
+        messages.empty? ? 'No changes for any HappyHood' : messages
       end
 
       def self.monthly_message
         neighborhood_differences = DifferenceCalculator.new(Hood.all, start_date: 1.month.ago, end_date: Date.today).differences
         messages  = DifferenceRenderer.summarize_differences(neighborhood_differences)
 
-        messages = messages.empty? ? "Could not calculate monthly difference" : messages
-
-        {
-          text:       messages,
-          icon_emoji: DefaultIconEmoji,
-          channel:    DefaultSlackChannel,
-        }
+        messages.empty? ? "Could not calculate monthly difference" : messages
       end
     end
   end
