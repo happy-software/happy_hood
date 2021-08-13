6f21de9bee294dfc2ee1f5b31f9df450ccd4c660
Hector Rios <that.hector@gmail.com>
2021-08-13 14:29:35 -0500

    New generic summary sender method + specs
    This commit adds a generic summary sender method to the Slack client.
    This is so that we can support any arbitrary windows of time, without
    needing to provide specific methods to represent this.

--- lib/happy_hood/slack/client.rb
+++ lib/happy_hood/slack/client.rb
@@ -24,6 +24,20 @@ module HappyHood
         })
       end
 
+      def self.send_summary(start_date:, end_date:)
+        message = build_message_for(start_date, end_date)
+
+        if message.empty?
+          message = "Could not calculate difference for dates #{start_date} through #{end_date}"
+        end
+
+        slack.chat_postMessage({
+          text: message,
+          icon_emoji: DefaultIconEmoji,
+          channel: DefaultSlackChannel,
+        })
+      end
+
       private
 
       def self.slack
@@ -42,6 +56,12 @@ module HappyHood
         messages  = DifferenceRenderer.summarize_differences(neighborhood_differences)
 
         messages.empty? ? "Could not calculate monthly difference" : messages
+      end
+
+      def self.build_message_for(start_date, end_date)
+        differences = DifferenceCalculator.new(Hood.all, start_date: start_date, end_date: end_date).differences
+
+        DifferenceRenderer.summarize_differences(differences)
       end
     end
   end
--- spec/lib/happy_hood/slack/client_spec.rb
+++ spec/lib/happy_hood/slack/client_spec.rb
@@ -81,4 +81,46 @@ describe HappyHood::Slack::Client do
       end
     end
   end
+
+  describe ".send_summary" do
+    let(:mock_slack_client) { instance_double(Slack::Web::Client) }
+    let(:start_date) { (Date.today - 1.day).beginning_of_quarter }
+    let(:end_date) { (Date.today - 1.day).end_of_quarter }
+
+    before do
+      allow(mock_slack_client).to receive(:chat_postMessage)
+      allow(Slack::Web::Client).to receive(:new).and_return(mock_slack_client)
+    end
+
+    context "when the neighborhoods have not had any valuation changes" do
+      it "posts a message saying no updates" do
+        mock_difference = instance_double(HoodDifference, empty?: true)
+        mock_calculator = instance_double(DifferenceCalculator, differences: [mock_difference])
+        allow(DifferenceCalculator).to receive(:new).and_return(mock_calculator)
+
+        described_class.send_summary(start_date: start_date, end_date: end_date)
+
+        expect(mock_slack_client).to have_received(:chat_postMessage).with(a_hash_including(
+          text: a_string_including("Could not calculate difference"),
+          icon_emoji: HappyHood::Slack::Client::DefaultIconEmoji,
+          channel: HappyHood::Slack::Client::DefaultSlackChannel,
+        ))
+      end
+    end
+
+    context "when there is a summary available" do
+      it "posts a message with the differences" do
+        mocked_response = "A ton of houses updated!"
+        allow(DifferenceRenderer).to receive(:summarize_differences).and_return(mocked_response)
+
+        described_class.send_summary(start_date: start_date, end_date: end_date)
+
+        expect(mock_slack_client).to have_received(:chat_postMessage).with(a_hash_including(
+          text: mocked_response,
+          icon_emoji: HappyHood::Slack::Client::DefaultIconEmoji,
+          channel: HappyHood::Slack::Client::DefaultSlackChannel,
+        ))
+      end
+    end
+  end
 end
