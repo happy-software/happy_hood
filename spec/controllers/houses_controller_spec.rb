171c1cff65caaf68ed7f623b57fe90295cd1f5cf
Hebron George <hebrontgeorge@gmail.com>
2021-08-22 01:20:14 -0400

    Include mocked api token

--- spec/controllers/houses_controller_spec.rb
+++ spec/controllers/houses_controller_spec.rb
@@ -10,11 +10,20 @@ describe HousesController, type: :request do
       '2021-01-02' => 124,
     }
   end
+  let(:mock_api_token) { "mock-token" }
+  let(:encrypted_mock_api_token) { ActionController::HttpAuthentication::Token.encode_credentials(mock_api_token) }
+
+  before do
+    allow(ENV).to receive(:[]).and_call_original
+    allow(ENV).to receive(:fetch).and_call_original
+    allow(ENV).to receive(:[]).with("HAPPY_HOUSE_API_TOKEN").and_return(mock_api_token)
+  end
 
   describe 'GET valuations' do
     context 'with zpid that does not exist' do
       it 'returns an error message' do
-        get '/house_valuations/xxx'
+        get '/house_valuations/xxx',
+            headers: { "HTTP_AUTHORIZATION" => encrypted_mock_api_token }
         expect(response).to_not be_successful
         expect(response.status).to eq(404)
       end
@@ -22,7 +31,8 @@ describe HousesController, type: :request do
 
     context 'with an existing house' do
       it 'should return the price history' do
-        get "/house_valuations/#{house_metadatum.zpid}"
+        get "/house_valuations/#{house_metadatum.zpid}",
+            headers: { "HTTP_AUTHORIZATION" => encrypted_mock_api_token }
         expect(response).to be_successful
         expect(response.status).to eq(200)
         expect(JSON.parse(response.body).dig('data')).to eq(history)
@@ -33,7 +43,8 @@ describe HousesController, type: :request do
   describe 'POST valuations' do
     context 'with a zpid that does not exist' do
       it 'returns an error message' do
-        post '/house_valuations/xxx'
+        post '/house_valuations/xxx',
+             headers: { "HTTP_AUTHORIZATION" => encrypted_mock_api_token }
         expect(response).to_not be_successful
         expect(response.status).to eq(404)
       end
@@ -48,7 +59,9 @@ describe HousesController, type: :request do
       end
       it 'should append imported data to house.price_history' do
         expect(house.price_history).to eq(history)
-        post "/house_valuations/#{house_metadatum.zpid}", params: {valuations: valuations_to_import}
+        post "/house_valuations/#{house_metadatum.zpid}",
+             headers: { "HTTP_AUTHORIZATION" => encrypted_mock_api_token },
+             params: {valuations: valuations_to_import}
         expect(house.reload.price_history).to eq(history.merge(JSON.parse valuations_to_import))
         expect(response.status).to eq(200)
       end
@@ -56,7 +69,9 @@ describe HousesController, type: :request do
 
     context 'without any valuations passed in' do
       it 'returns an error message' do
-        post "/house_valuations/#{house_metadatum.zpid}", params: {}
+        post "/house_valuations/#{house_metadatum.zpid}",
+             headers: { "HTTP_AUTHORIZATION" => encrypted_mock_api_token },
+             params: {}
         expect(response).not_to be_successful
         expect(response.status).to eq(400)
       end
