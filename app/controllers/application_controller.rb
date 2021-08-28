2306c71ff92fbaead38714563c1caeba079cba23
Hebron George <hebrontgeorge@gmail.com>
2021-08-28 13:25:26 -0400

    More logs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,10 @@ class ApplicationController < ActionController::Base
 
   def authenticate_request(key)
     Rails.logger.info("Hello Hebron, an authentication request has come in for key: #{key}")
-    puts "[puts] Hello Hebron, an authentication request has come in for key: #{key}"
+    is_match = ActiveSupport::SecurityUtils.secure_compare(token, key)
+    Rails.logger.info("Hello Hebron, is_match =#{is_match}")
     authenticate_or_request_with_http_token do |token, _options|
       Rails.logger.info("Hello Hebron, their token is: #{token}")
-      puts "[puts] Hello Hebron, their token is: #{token}"
-      is_match = ActiveSupport::SecurityUtils.secure_compare(token, key)
-      Rails.logger.info("Hello Hebron, is_match =#{is_match}")
       ActiveSupport::SecurityUtils.secure_compare(token, key)
     end
   end
